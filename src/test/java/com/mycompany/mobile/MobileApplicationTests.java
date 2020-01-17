9d6462d460605b2c5c2fa45fc14c5f73724289ac
Joe Cloe <joe.cloe@mycompany.com>
2020-01-17 18:49:12 -0300

    PM-2: Creating a new branch.

---
+++ src/test/java/com/mycompany/mobile/MobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
